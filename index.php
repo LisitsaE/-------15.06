c148976decf7be0d850267bd985d5914ffa72b7e
Lisitsa_E <lisitsa_e@outlook.com>
2022-06-26 14:31:44 +0700

    1

--- index.php
+++ index.php
@@ -4,27 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My first site</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <header class="col-sm-12 col-md-12 col-lg-12"></header>
+            <header class="cool-sm-12 col-md-12 col-lg-12">
 
+            </header>
         </div>
-        
+        <div class="row">
+            <nav class="cool-sm-12 col-md-12 col-lg-12">
+                <?php
+                    include_once "pages/menu.php";
+                    include_once "pages/functions.php";
 
-    
-    
-    
-    
-    
-    
+                ?>
+
+            </nav>
+        </div>
+        <div class="row">
+            <section class="cool-sm-12 col-md-12 col-lg-12">
+                <?php
+                    if (isset($_GET['page'])) {
+                        $page = $_GET['page'];
+                        if ($page == 1) include_once "page/home.php";
+                        if ($page == 2) include_once "page/upload.php";
+                        if ($page == 3) include_once "page/gallery.php";
+                        if ($page == 4) include_once "page/registration.php";
+                    }
+                ?>
+            </section>
+        </div>
     </div>
-
-<script></script>
-<script src>"js/bootstrap.min.js"</script>
+    <script src="https://ajax.googleleapis.com/ajax/libs/jqery/1.12.4/jqery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     
 </body>
 </html>
--- page/menu.php
+++ page/menu.php
@@ -3,7 +3,12 @@
         <a href="index.php?page=1">Home</a>
     </li>
     <li>
-
+    <a href="index.php?page=2">Upload</a>
     </li>
-
+    <li>
+    <a href="index.php?page=3">Gallery</a>
+    </li>
+    <li>
+    <a href="index.php?page=4">Registration/a>
+    </li>
 </ul>
